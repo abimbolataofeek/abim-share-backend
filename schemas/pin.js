4ddb8abed37d7140648e9160fc12d01d3a5dff3d
abimbolataofeek <65461446+abimbolataofeek@users.noreply.github.com>
2022-03-02 13:50:56 +0100

    initial commit schemas created

---
+++ schemas/pin.js
@@ -0,0 +1,59 @@
+export default {
+    name: 'pin',
+    title: 'pin',
+    type: 'document',
+    fields: [
+        {
+            name: 'title',
+            title: 'Title',
+            type: 'string',
+        },
+        {
+            name: 'about',
+            title: 'About',
+            type: 'string',
+        },
+        {
+            name: 'destination',
+            title: 'Destination',
+            type: 'url',
+        },
+        {
+            name: 'category',
+            title: 'Category',
+            type: 'url',
+        },
+        {
+            name: 'image',
+            title: 'Image',
+            type: 'image',
+            option:{
+                hotspot: true
+            }
+        },
+        {
+            name: 'userId',
+            title: 'UserId',
+            type: 'string',
+        },
+        {
+            name: 'postedBy',
+            title: 'postedBy',
+            type: 'postedBy',
+        },
+        {
+            name: 'save',
+            title: 'Save',
+            type: 'array',
+            of: [{type: 'save'}]
+        },
+        {
+            name: 'comments',
+            title: 'Comments',
+            type: 'array',
+            of: [{type: 'comment'}]
+        },
+
+
+    ]
+}
